54c2df2992d341c24a530867115be3ac7e2e0653
Fernanda Pereira <fernandacarolina.fcsp@gmail.com>
2021-05-07 11:39:43 -0300

    learning about conditions in js

---
+++ aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 20
+console.log(`The velocity of your car is ${vel} Km/h`)
+if (vel > 60) { //Condição simples
+    console.log(`You exceeded the allowed velocity. MULTADO!`)
+}
+console.log(`Always drive with seat belts!`)
---
+++ aula11/ex009.js
@@ -0,0 +1,7 @@
+var country = 'Georgia'
+console.log(`Living in ${country}`)
+if (country != 'Brasil') { //Condição composta
+    console.log('You are foreign!')
+} else {
+    console.log('You are brasilian!')    
+}
