60bd0b9bb620baeb7662e1e4bd8477100532c57c
Fernanda Pereira <fernandacarolina.fcsp@gmail.com>
2021-05-19 15:52:15 -0300

    class 15 and 16 functions in js

---
+++ class16/function.js
@@ -0,0 +1,8 @@
+function PairOrOdd(n) {
+    if (n%2 == 0) {
+        return "Pair"
+    } else {
+        return "Odd"
+    }
+}
+console.log(PairOrOdd(5))
---
+++ class16/function2.js
@@ -0,0 +1,5 @@
+let sum = function (n1=0, n2=0) {
+    return n1+n2
+}
+console.log(sum(5, 10))
+console.log(sum(5))
